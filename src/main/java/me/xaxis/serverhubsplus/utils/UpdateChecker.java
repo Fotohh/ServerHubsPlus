b4d7f0ed46d7130f9d992fcbc432fb5c6891b335
XaxisPlayz <66885524+XaxisPlayz@users.noreply.github.com>
2022-09-07 16:10:00 -0700

    update checker

--- src/main/java/me/xaxis/serverhubsplus/utils/UpdateChecker.java
+++ src/main/java/me/xaxis/serverhubsplus/utils/UpdateChecker.java
@@ -1,4 +1,15 @@
 package me.xaxis.serverhubsplus.utils;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class UpdateChecker {
+
+    public static void main(String[] args) throws IOException {
+
+        URL url = new URL("Server returned HTTP response code: 503 for URL:");
+
+
+    }
+
 }
